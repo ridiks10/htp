640f517d066f8db4581bf1c81735f53dd719dcdb
tailoc <tailoc@icsc.vn>
2016-09-08 02:11:11 +0700

    asf

--- catalog/controller/home/page.php
+++ catalog/controller/home/page.php
@@ -1,13 +1,83 @@
 <?php
 class ControllerHomePage extends Controller {
 	public function index() {
-
+$data['route'] = $this -> request ->get['route'];
 		$data['base'] = HTTPS_SERVER;
 		$data['self'] = $this;
-		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/page.tpl')) {
-			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/page.tpl', $data));
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/home.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/home.tpl', $data));
 		} else {
-			$this -> response -> setOutput($this -> load -> view('default/template/home/page.tpl', $data));
+			$this -> response -> setOutput($this -> load -> view('default/template/home/home.tpl', $data));
+		}
+	}
+	public function news() {
+$data['route'] = $this -> request ->get['route'];
+		$data['base'] = HTTPS_SERVER;
+		$data['self'] = $this;
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/news.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/home.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/home/news.tpl', $data));
+		}
+	}
+	public function project() {
+$data['route'] = $this -> request ->get['route'];
+		$data['base'] = HTTPS_SERVER;
+		$data['self'] = $this;
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/project.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/project.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/home/project.tpl', $data));
+		}
+	}
+	public function parther() {
+$data['route'] = $this -> request ->get['route'];
+		$data['base'] = HTTPS_SERVER;
+		$data['self'] = $this;
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/parther.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/parther.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/home/parther.tpl', $data));
+		}
+	}
+	public function faq() {
+$data['route'] = $this -> request ->get['route'];
+		$data['base'] = HTTPS_SERVER;
+		$data['self'] = $this;
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/faq.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/faq.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/home/faq.tpl', $data));
+		}
+	}
+	public function detail_news() {
+$data['route'] = $this -> request ->get['route'];
+		$data['base'] = HTTPS_SERVER;
+		$data['self'] = $this;
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/detail_news.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/detail_news.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/home/detail_news.tpl', $data));
+		}
+	}
+	public function contact() {
+$data['route'] = $this -> request ->get['route'];
+		$data['base'] = HTTPS_SERVER;
+		$data['self'] = $this;
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/contact.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/contact.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/home/contact.tpl', $data));
+		}
+	}
+	public function about() {
+		$data['route'] = $this -> request ->get['route'];
+		$data['base'] = HTTPS_SERVER;
+		$data['self'] = $this;
+		if (file_exists(DIR_TEMPLATE . $this -> config -> get('config_template') . '/template/home/about.tpl')) {
+			$this -> response -> setOutput($this -> load -> view($this -> config -> get('config_template') . '/template/home/about.tpl', $data));
+		} else {
+			$this -> response -> setOutput($this -> load -> view('default/template/home/about.tpl', $data));
 		}
 	}
 }
