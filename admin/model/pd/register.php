7ea0ef25896c09a585dffdb57eb518d17f276b44
tailoc <tailoc@icsc.vn>
2016-09-11 15:30:23 +0700

    qe

--- admin/model/pd/register.php
+++ admin/model/pd/register.php
@@ -653,7 +653,8 @@ $p_binary= $p_binary['customer_id'];
 	}
 
 	public function getRightO($id) {
-		$query = $this -> db -> query('select u2.email, u2.telephone,u2.date_added, mlm.customer_id as id, mlm.level,CONCAT(u2.firstname," (ĐT: ",u2.telephone,")") as text, CONCAT( "level1"," right") as iconCls,CONCAT(u2.firstname," (ĐT: ",u2.telephone,")") as name,l.name_vn as level_user,u2.username,u2.status,u2.date_added from ' . DB_PREFIX . 'customer AS u2 LEFT join ' . DB_PREFIX . 'customer_ml AS mlm ON u2.customer_id = mlm.customer_id INNER join ' . DB_PREFIX . 'customer_ml AS u1 ON u1.right = mlm.customer_id left Join ' . DB_PREFIX . 'member_level as l ON l.id = mlm.level where mlm.p_binary = ' . (int)$id);
+		$query = $this -> db -> query('select u2.email, u2.telephone,u2.date_added, u2.account_holder as account_holder,
+			mlm.customer_id as id, mlm.level,CONCAT(u2.firstname," (ĐT: ",u2.telephone,")") as text, CONCAT( "level1"," right") as iconCls,CONCAT(u2.firstname," (ĐT: ",u2.telephone,")") as name,l.name_vn as level_user,u2.username,u2.status,u2.date_added from ' . DB_PREFIX . 'customer AS u2 LEFT join ' . DB_PREFIX . 'customer_ml AS mlm ON u2.customer_id = mlm.customer_id INNER join ' . DB_PREFIX . 'customer_ml AS u1 ON u1.right = mlm.customer_id left Join ' . DB_PREFIX . 'member_level as l ON l.id = mlm.level where mlm.p_binary = ' . (int)$id);
 		//return json_decode(json_encode($query->row), false);
 		return $query -> row;
 	}
