6d521e9a3fe1a416a323b6003e619216ed3ca096
tailoc <tailoc@icsc.vn>
2016-09-11 14:58:51 +0700

    a

--- catalog/controller/account/dashboard.php
+++ catalog/controller/account/dashboard.php
@@ -636,29 +636,29 @@ if ($getLanguage == 'vietnamese') {
 		{
 			
 			if (doubleval($getgoidautu['filled']) < 500000000){
-				$json['phantram'] = intval($count['total_pd_right'])*0.1;
+				$total = intval($count['total_pd_right'])*0.1;
 			}
 			if (doubleval($getgoidautu['filled']) >= 500000000 && doubleval($getgoidautu['filled']) < 1000000000){
-				$json['phantram'] = intval($count['total_pd_right'])*0.11;
+				$total = intval($count['total_pd_right'])*0.11;
 			}
 			if (doubleval($getgoidautu['filled']) >= 10000000000){
-				$json['phantram'] = intval($count['total_pd_right'])*0.12;
+				$total = intval($count['total_pd_right'])*0.12;
 			}
 		}
 		else
 		{
 			
 			if (doubleval($getgoidautu['filled']) < 500000000){
-				$json['phantram'] = intval($count['total_pd_left'])*0.1;
+				$total = intval($count['total_pd_left'])*0.1;
 			}
 			if (doubleval($getgoidautu['filled']) >= 500000000 && doubleval($getgoidautu['filled']) < 1000000000){
-				$json['phantram'] = intval($count['total_pd_left'])*0.11;
+				$total = intval($count['total_pd_left'])*0.11;
 			}
 			if (doubleval($getgoidautu['filled']) >= 10000000000){
-				$json['phantram'] = intval($count['total_pd_left'])*0.12;
+				$total = intval($count['total_pd_left'])*0.12;
 			}
 		}
-			$json_phantram['phantram'] = number_format($json['phantram']);
+		$json_phantram['phantram'] = number_format($total);
 		$this -> response -> setOutput(json_encode($json_phantram));
 
 	}
