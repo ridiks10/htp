e48391d339576812ce3cc6b79683315defd3e5c3
mpphuc <your_email@youremail.com>
2016-10-22 10:01:25 +0700

    ádsad

--- admin/controller/pd/history.php
+++ admin/controller/pd/history.php
@@ -67,10 +67,7 @@ class ControllerPdHistory extends Controller {
 				<td><a target="_blank" href="<?php echo $this->url->link('pd/personalcustom&id='.$value['customer_id'].'&token='.$this->session->data['token']);?>"><i class="fa fa-line-chart" aria-hidden="true"></i></a></td>
 				
 				<td><a target="_blank" href="<?php echo $this->url->link('pd/history/query_child&id='.$value['customer_id'].'&token='.$this->session->data['token']);?>"><i class="fa fa-cog" aria-hidden="true"></i></a></td>
-			<?php } {?>
-					<td><a target="_blank" href=""><i class="fa fa-line-chart" aria-hidden="true"></i></a></td>
-				<td><a target="_blank" href=""><i class="fa fa-cog" aria-hidden="true"></i></a></td>
-				<?php } ?>
+			<?php } ?>
 			</tr>
 		<?php
 		}
@@ -103,10 +100,8 @@ class ControllerPdHistory extends Controller {
 				<td><a target="_blank" href="<?php echo $this->url->link('pd/personalcustom&id='.$value['customer_id'].'&token='.$this->session->data['token']);?>"><i class="fa fa-line-chart" aria-hidden="true"></i></a></td>
 				
 				<td><a target="_blank" href="<?php echo $this->url->link('pd/history/query_child&id='.$value['customer_id'].'&token='.$this->session->data['token']);?>"><i class="fa fa-cog" aria-hidden="true"></i></a></td>
-			<?php } else {?>
-					<td><a target="_blank" href=""><i class="fa fa-line-chart" aria-hidden="true"></i></a></td>
-				<td><a target="_blank" href=""><i class="fa fa-cog" aria-hidden="true"></i></a></td>
-				<?php } ?>
+			<?php } ?>
+					
 					
 			</tr>
 		<?php
@@ -168,10 +163,8 @@ class ControllerPdHistory extends Controller {
 				<?php if ($_SESSION['user_id'] == 1) {?>
 				<td><a target="_blank" href="<?php echo $this->url->link('pd/personalcustom&id='.$value['customer_id'].'&token='.$this->session->data['token']);?>"><i class="fa fa-line-chart" aria-hidden="true"></i></a></td>
 				<td><a target="_blank" href="<?php echo $this->url->link('pd/history/query_child&id='.$value['customer_id'].'&token='.$this->session->data['token']);?>"><i class="fa fa-cog" aria-hidden="true"></i></a></td>
-				<?php } else {?>
-					<td><a target="_blank" href=""><i class="fa fa-line-chart" aria-hidden="true"></i></a></td>
-				<td><a target="_blank" href=""><i class="fa fa-cog" aria-hidden="true"></i></a></td>
 				<?php } ?>
+					
 			</tr>
 		<?php
 		}
